3cd6378917da5762b0041724a1ea1b1bec05422f
gsaud896 <gsaud896@gmail.com>
2024-07-09 09:51:44 +0545

    route update

--- server/src/routes/completed.ts
+++ server/src/routes/completed.ts
@@ -6,7 +6,7 @@ const router = express();
 //url = http://localhost:3000/api/completed method = GET
 router.get("/", getAllCompletedTodos);
 
-//url = http://localhost:3000/api/completed/1 method = GET
-router.get("/:id", completedTodos);
+//url = http://localhost:3000/api/completed/1 method = PUT
+router.put("/:id", completedTodos);
 
 export default router;
