73218eabf4c97ae87092f9e263f0da1f081bc27c
gsaud896 <gsaud896@gmail.com>
2024-07-09 13:38:09 +0545

    comment updated

--- server/src/routes/completed.ts
+++ server/src/routes/completed.ts
@@ -3,10 +3,10 @@ import { completedTodos, getAllCompletedTodos } from "../controller/todo";
 
 const router = express();
 
-//url = http://localhost:3000/api/completed method = GET
+//url = http://localhost:3000/api/completedTodos method = GET
 router.get("/", getAllCompletedTodos);
 
-//url = http://localhost:3000/api/completed/1 method = PUT
+//url = http://localhost:3000/api/completedTodos/1 method = PUT
 router.put("/:id", completedTodos);
 
 export default router;
